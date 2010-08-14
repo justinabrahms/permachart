9939748987411175fcbfb9fe17907e5a52dd6ae1
Justin Lilly <justin@justinlilly.com>
2010-08-14 13:44:39 -0400

    all unknown things should goto chart_detail (for now)

--- permachart/urls.py
+++ permachart/urls.py
@@ -4,5 +4,6 @@ from charter import views as charter_views
 urlpatterns = patterns(
     '',
     url('^$', charter_views.home, name="home"),
-    url('^new/$', charter_views.manual_data_import, name="new-chart"),
+    url('^new/$', charter_views.manual_data_import, name="chart-new"),
+    url('^(.*)/$', charter_views.chart_detail, name="chart-detail"),
 )
