45a471f12049b64ff9fad802cdbac0b974f53c65
Justin Lilly <justin@justinlilly.com>
2010-08-14 18:22:22 -0400

    pretty url encoding

--- permachart/charter/utils.py
+++ permachart/charter/utils.py
@@ -3,6 +3,7 @@ from charter.models import DataRow
 from urllib import quote
 from urlparse import urlunparse
 
+ALPHABET="23456789abcdefghijkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ"
 
 _cht = dict({
     'pie': 'p3',
@@ -36,3 +37,39 @@ def get_graph_url(dataset,cht='p3'):
         query,
         ''
     ))
+
+def pretty_encode(num, alphabet=ALPHABET):
+    """Encode a number in Base X
+
+    `num`: The number to encode
+    `alphabet`: The alphabet to use for encoding
+    """
+    if (num == 0):
+        return alphabet[0]
+    arr = []
+    base = len(alphabet)
+    while num:
+        rem = num % base
+        num = num // base
+        arr.append(alphabet[rem])
+    arr.reverse()
+    return ''.join(arr)
+
+def pretty_decode(string, alphabet=ALPHABET):
+    """Decode a Base X encoded string into the number
+
+    Arguments:
+    - `string`: The encoded string
+    - `alphabet`: The alphabet to use for encoding
+    """
+    base = len(alphabet)
+    strlen = len(string)
+    num = 0
+
+    idx = 0
+    for char in string:
+        power = (strlen - (idx + 1))
+        num += alphabet.index(char) * (base ** power)
+        idx += 1
+
+    return num
