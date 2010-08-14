548c395119f2afd5dd409a10261bdc520902cf34
Justin Lilly <justin@justinlilly.com>
2010-08-14 11:45:21 -0400

    Cleaning up suggested main.py for django apps.

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ import logging, os, sys
 # Google App Engine imports.
 from google.appengine.ext.webapp import util
 
-# Remove the standard version of Django.
+# # Remove the standard version of Django.
 for k in [k for k in sys.modules if k.startswith('django')]:
     del sys.modules[k]
 
-# Force sys.path to have our own directory first, in case we want to import
-# from it.
+# # Force sys.path to have our own directory first, in case we want to import
+# # from it.
 sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 for path, dirs, files in os.walk(os.path.join(os.path.dirname(__file__), 'deps')):
     for dir in dirs:
@@ -19,26 +19,15 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'permachart.settings'
 
 import django.core.handlers.wsgi
 import django.core.signals
-import django.db
-import django.dispatch.dispatcher
 
 def log_exception(*args, **kwds):
    logging.exception('Exception in request:')
 
 # Log errors.
-django.dispatch.dispatcher.connect(
-    log_exception, django.core.signals.got_request_exception)
-
-# Unregister the rollback event handler.
-django.dispatch.dispatcher.disconnect(
-    django.db._rollback_on_exception,
-    django.core.signals.got_request_exception)
+django.core.signals.got_request_exception.connect(log_exception)
 
 def main():
-    # Create a Django application for WSGI.
     application = django.core.handlers.wsgi.WSGIHandler()
-
-    # Run the WSGI CGI handler with that application.
     util.run_wsgi_app(application)
 
 if __name__ == '__main__':
