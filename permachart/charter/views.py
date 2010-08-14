3cd4bec9017654180503319018a31e8c42adcb4e
Justin Lilly <justin@justinlilly.com>
2010-08-14 13:44:01 -0400

    some basic, albeit incorrect, views

--- permachart/charter/views.py
+++ permachart/charter/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render_to_response
-from charter.models import Chart# , ChartDataSet, DataRow
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+from django.shortcuts import render_to_response, get_object_or_404
+
+from charter.models import Chart, ChartDataSet, DataRow
+from charter.forms import ChartForm
 
 def home(request):
     return render_to_response('home.html')
@@ -10,14 +14,21 @@ def signin(request):
 
 def manual_data_import(request):
     if request.method == "POST":
-        pass
-    pass
+        cf = ChartForm(request.POST)
+        if cf.is_valid():
+            ident = cf.save()
+            return HttpResponseRedirect(reverse('chart-detail', args=(chart.id,)))
+    else:
+        cf = ChartForm()
+    return render_to_response('charter/data_input.html', {'f':cf})
 
 def bulk_data_import(request):
     pass
 
-def chart_detail(request):
-    pass
+def chart_detail(request, id):
+    chart = get_object_or_404(Chart, id=id)
+    return render_to_response('charter/detail.html', {'chart':chart})
+
 
 def chart_list(request):
     pass
