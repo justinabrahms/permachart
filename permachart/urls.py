ce977264c42c3a6d0ecd4b754fefd7ea143ff226
Justin Lilly <justin@justinlilly.com>
2010-08-14 13:13:05 -0400

    hooking in some urls

--- permachart/urls.py
+++ permachart/urls.py
@@ -1,17 +1,8 @@
 from django.conf.urls.defaults import *
+from charter import views as charter_views
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
-
-urlpatterns = patterns('',
-    # Example:
-    # (r'^permachart/', include('permachart.foo.urls')),
-
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
+urlpatterns = patterns(
+    '',
+    url('^$', charter_views.home, name="home"),
+    url('^new/$', charter_views.manual_data_import, name="new-chart"),
 )
