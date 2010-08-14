0625c112918c93997da543973ba536665d1c768b
Justin Lilly <justin@justinlilly.com>
2010-08-14 18:22:16 -0400

    chart dataset form

--- permachart/charter/forms.py
+++ permachart/charter/forms.py
@@ -1,7 +1,12 @@
 from google.appengine.ext.db import djangoforms
-from charter.models import Chart
+from charter.models import Chart, ChartDataSet
 
 class ChartForm(djangoforms.ModelForm):
     class Meta:
         model = Chart
         exclude = ('data', 'user', 'hash',)
+
+class DataSetForm(djangoforms.ModelForm):
+    class Meta:
+        model = ChartDataSet
+        exclude = ('previous_version',)
