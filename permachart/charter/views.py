c5a27e0a1ae86f459f48a8437ec277c9d4e434b5
Justin Lilly <justin@justinlilly.com>
2010-08-14 14:23:04 -0400

    View fleshing

--- permachart/charter/views.py
+++ permachart/charter/views.py
@@ -1,5 +1,5 @@
 from django.core.urlresolvers import reverse
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, Http404
 from django.shortcuts import render_to_response, get_object_or_404
 
 from charter.models import Chart, ChartDataSet, DataRow
@@ -16,8 +16,8 @@ def manual_data_import(request):
     if request.method == "POST":
         cf = ChartForm(request.POST)
         if cf.is_valid():
-            ident = cf.save()
-            return HttpResponseRedirect(reverse('chart-detail', args=(chart.id,)))
+            chart = cf.save()
+            return HttpResponseRedirect(reverse('chart-detail', args=(chart.hash,)))
     else:
         cf = ChartForm()
     return render_to_response('charter/data_input.html', {'f':cf})
@@ -25,13 +25,16 @@ def manual_data_import(request):
 def bulk_data_import(request):
     pass
 
-def chart_detail(request, id):
-    chart = get_object_or_404(Chart, id=id)
+def chart_detail(request, hash):
+    try:
+        chart = Chart.get(hash=hash)
+    except:
+        raise Http404
     return render_to_response('charter/detail.html', {'chart':chart})
 
-
 def chart_list(request):
-    pass
+    chart_list = Chart.all()
+    return render_to_response('charter/list.html', {'chart_list':chart_list})
 
 def get_chart_resource(request):
     pass
