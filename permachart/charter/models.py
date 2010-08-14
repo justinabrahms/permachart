c9a733d3b089c8b4b8932b2fa0aaa3a1fb2cd1ed
Justin Lilly <justin@justinlilly.com>
2010-08-14 15:24:38 -0400

    Model.put, not save for overriding purposes.

--- permachart/charter/models.py
+++ permachart/charter/models.py
@@ -22,7 +22,7 @@ class Chart(db.Model):
     def __unicode__(self):
         return "%s - %s" % (self.name, self.chart_type)
 
-    def save(self, *args, **kwargs):
+    def put(self, *args, **kwargs):
         # FIXME: Bad hashing key.
-        self.hash = hashlib.md5.hash(name).hexdigest()
-        super(Chart, self).save(*args, **kwargs)
+        self.hash = hashlib.md5(self.name).hexdigest()
+        super(Chart, self).put(*args, **kwargs)
