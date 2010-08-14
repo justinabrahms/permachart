804f90faa58d5454aefefee8815e9ddf7322d620
Issac Kelly <issac.kelly@gmail.com>
2010-08-14 16:53:27 -0400

    use real numbers 0-100 for pie charts

--- permachart/charter/utils.py
+++ permachart/charter/utils.py
@@ -5,7 +5,7 @@ from urlparse import urlunparse
 
 
 _cht = dict({
-    'pie': 'p',
+    'pie': 'p3',
 })
 
 def get_graph_url(dataset,cht='p3'):
@@ -13,12 +13,18 @@ def get_graph_url(dataset,cht='p3'):
     data = dict()
     data['cht'] = cht
     data['chs'] = '600x480'
-    chd = []
+    values = []
     chl = []
+    chd = []
+    summation = 0
     for data_item in dataset.data_rows:
         row = DataRow.get(data_item)
-        chd.append(row.data_value)
+        values.append(float(row.data_value))
         chl.append(row.data_key)
+    for value in values:
+        summation += value
+    for value in values:
+        chd.append(str(value/summation))
     data['chd'] = 't:' + ','.join(chd)
     data['chl'] = '|'.join(chl)
     query = '&'.join([k+'='+quote(str(v)) for (k,v) in data.items()])
