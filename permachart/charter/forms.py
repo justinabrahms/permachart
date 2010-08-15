a84d734c52337f8d62b02cd1aec3ab8d4493c554
Justin Lilly <justin@justinlilly.com>
2010-08-15 09:15:55 -0400

    Django can handle its own querydict decompisiton if we provide the proper prefixes. *sigh*

--- permachart/charter/forms.py
+++ permachart/charter/forms.py
@@ -33,32 +33,13 @@ class DataRowFormSet(object):
          - altering the key
          - altering the value
         """
-        formset_dict = defaultdict(QueryDict)
         self.forms = []
-            
-        if data:
-            if not hasattr(data, '__iter__'):
-                raise KeyError("data attribute must be an iterable.")
-            if len(instances) == len(data):
-                raise KeyError("data and instances iterables must be the same length")
-
-            data._mutable = True
-            for key, val in dict(data).iteritems():
-                data.pop(key)
-                num, key = key.split('-', 1)
-                if num in formset_dict:
-                    formset_dict[num].update({key:val})
-                else:
-                    formset_dict[num] = QueryDict({key:val})
-
-            # FIXME: Validate that data and instances will always return in the same order
-            for i, obj in enumerate(instances):
-                if isinstance(obj, db.Key):
-                    # fetch keys for validation
-                    # FIXME: Can probably do a bulk get
-                    obj = db.get(obj)
-                data = formset_dict[str(i)]
-
+        for i, obj in enumerate(instances):
+            if isinstance(obj, db.Key):
+                # fetch keys for validation
+                # FIXME: Can probably do a bulk get
+                obj = db.get(obj)
+            if data:
                 self.forms.append(
                     self.base_form(
                         instance=obj,
@@ -66,17 +47,8 @@ class DataRowFormSet(object):
                         prefix="%i" % i,
                     )
                 )
-        else:
-            for i, obj in enumerate(instances):
-                if isinstance(obj, db.Key):
-                    # fetch keys for validation
-                    # FIXME: Can probably do a bulk get
-                    obj = db.get(obj)
-                try:
-                    data = formset_dict[i]
-                except IndexError:
-                    data = None
 
+            else:
                 self.forms.append(
                     self.base_form(
                         instance=obj,
